a278ca61d13be2ce195d77e28952892e2d12e950
Syun <syunk38@gmail.com>
2017-05-12 16:59:33 +0900

    chromeをヘッドレスモードで使う

--- karma.conf.js
+++ karma.conf.js
@@ -8,9 +8,15 @@ module.exports = function(config) {
             'app/app.js': ['coverage'],
             'test/*Spec.js': ['espower']
         },
-        browsers: ['ChromeCanary'],
+        browsers: ['ChromeCanary_headless'],
         frameworks: ['mocha', 'power-assert'],
         reporters: ['mocha', 'coverage'],
+        customLaunchers: {
+            ChromeCanary_headless :{
+                base: 'ChromeCanary',
+                flags: ['--no-sandbox', '--headless', '--disable-gpu', ' --remote-debugging-port=9222']
+            }
+        },
         logLevel: config.LOG_DEBUG
     })
 }
