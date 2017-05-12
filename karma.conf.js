ccff5f12c4a377483c589c3240f867051ee90a54
Syun <syunk38@gmail.com>
2017-05-12 16:13:51 +0900

    karma + mocha + power-assertまで一通り

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,16 @@
 module.exports = function(config) {
     config.set({
         files: [
-            'app/app.js'
+            'app/app.js',
+            'test/*Spec.js'
         ],
+        preprocessors: {
+            'app/app.js': ['coverage'],
+            'test/*Spec.js': ['espower']
+        },
+        browsers: ['ChromeCanary'],
+        frameworks: ['mocha', 'power-assert'],
+        reporters: ['mocha', 'coverage'],
         logLevel: config.LOG_DEBUG
     })
 }
---
+++ test/appSpec.js
@@ -0,0 +1,11 @@
+describe('add関数のテスト', function() {
+    it('1 + 2 は 3', function() {
+        assert(add(1, 2) === 3)
+    });
+});
+
+describe('substract関数のテスト', function() {
+    it('3 - 2 は 1', function() {
+        assert(substract(3, 2) === 1)
+    });
+});
